399403b79f35915d897faa8f28c717c9e89d98cf
sunder2568223 <sunderyssr@gmail.com>
2023-07-25 15:44:19 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDataProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"D:\Projects\RainbowSchools.txt");
+            System.Console.WriteLine(text);
+            Console.ReadLine();
+
+        }
+    }
+}
